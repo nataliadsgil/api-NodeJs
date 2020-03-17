9371d28da767c827b0227eff884ed0524b27346f
Dms98Br <54338660+Dms98Br@users.noreply.github.com>
2020-03-17 18:42:27 -0300

    Update config.js

--- src/configure/config.js
+++ src/configure/config.js
@@ -3,7 +3,7 @@ global.SALT_KEY = 'f5b99242-6504-4ca3-90f2-05e78e5761ef';
 global.EMAIL_TMPL = 'Olá, <strong>{0}</strong> welcome to Profinder, agora você pode se candidatar as nossas vagas!!';
 
 module.exports = {
-    connectionString:'mongodb+srv://ProFinder:profinder@cluster0-tz03r.mongodb.net/test',
+    connectionString:'mongodb+srv://ProFinder:profinder@cluster0-y6z2e.azure.mongodb.net/test',
     sendgridKey: '',//Coloque seu código do sendgrid aqui    
     
     containerConnectionString: 'TBD'
