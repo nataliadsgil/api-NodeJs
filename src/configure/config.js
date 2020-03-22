a0f09cd23559a6c184ce408f715cfdfc042a7749
Dms98Br <54338660+Dms98Br@users.noreply.github.com>
2020-03-22 11:04:03 -0300

    gitignire salt_key

--- src/configure/config.js
+++ src/configure/config.js
@@ -1,5 +1,5 @@
 
-global.SALT_KEY = 'f5b99242-6504-4ca3-90f2-05e78e5761ef';
+global.SALT_KEY = '';
 global.EMAIL_TMPL = 'Olá, <strong>{0}</strong> welcome to Profinder, agora você pode se candidatar as nossas vagas!!';
 
 module.exports = {
