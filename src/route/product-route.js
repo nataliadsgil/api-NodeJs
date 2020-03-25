41020f417f6013a8765bf080f1207b325913d11a
Natália Silva <nataliadasilva.dev@gmail.com>
2020-03-25 01:31:52 +0000

    testando put, tirando auth service temp
    Signed-off-by: Natália Silva <nataliadasilva.dev@gmail.com>

--- src/route/product-route.js
+++ src/route/product-route.js
@@ -9,7 +9,7 @@ router.get('/:slug', controller.getBySlug);
 router.get('/tags/:tag', controller.getByTag);
 router.get('/admin/:id', controller.getById);//Diferente por causa de conflito de rotas
 router.post('/', authService.isAdmin, controller.post);
-router.put('/:id', authService.isAdmin, controller.put);
+router.put('/:id', controller.put);
 router.delete('/:id', authService.isAdmin, controller.delete);
 
 module.exports = router;
--- src/services/auth-service.js
+++ src/services/auth-service.js
@@ -32,6 +32,7 @@ exports.authorize = function (req, res, next){
 };
 
 exports.isAdmin = function(req, res, next){
+    
     var token = req.body.token || req.query.token || req.headers['x-access-token'];
 
     if(!token){
